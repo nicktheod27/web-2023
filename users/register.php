37a5ef20908227010701dbb1493fafca287c1ab1
nicktheod27 <127815182+nicktheod27@users.noreply.github.com>
2024-03-28 21:31:06 +0200

    Update register.php

--- users/register.php
+++ users/register.php
@@ -13,34 +13,43 @@
 </head>
 <body>
     <div class="register">
-        <?php 
-         include("php/config.php");
-         if(isset($_POST['sumbit'])){
-            $username = $_POST['username'];
-            $email = $_POST['email'];
-            $password = $_POST['password'];
-            $password_repeat = $_POST['password_repeat'];
-            //verifying unique email
+    <?php 
+include("php/config.php");
 
-            $verify_query = mysqli_query($conn,"SELECT email FROM users WHERE email='$email'");
+// Function to safely output JavaScript alerts
+function js_alert($message) {
+    echo "<script>alert('$message');</script>";
+}
 
-            if(mysqli_num_rows($verify_query) != 0){
-                echo "<div class='message'>
-                <p>This email is used, Try another One Please!</p>
-            </div> <br>";
-            echo "<a href='javascript:self.history.back()'><button class='btn'>Go Back</button>";
-            }
-            else{
-                mysqli_query($conn,"INSERT INTO users(Username,Email,Password) VALUES('$username','$email','$password')") or die("Error Occured");
-                echo "<div class='message'>
-                <p>Registration successfully!</p>
-            </div> <br>";
-            echo "<a href='login.php'><button class='btn'>Login Now </button>";
-            }
-         }else{
+if(isset($_POST['submit'])){
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $password_repeat = $_POST['password_repeat'];
+    
+    // Verifying unique email
+    $verify_query = mysqli_query($conn, "SELECT email FROM users WHERE email='$email'");
+    
+    if(mysqli_num_rows($verify_query) != 0){
+        header("Location: register.php?error=email_in_use");
+        exit();
+    } else {
+        mysqli_query($conn, "INSERT INTO users (Username, Email, Password) VALUES ('$username', '$email', '$password')") or die("Error Occurred");
+        header("Location: login.php?success=registration_successful");
+        exit(); 
+    }
+}
 
-    ?>
-        <img src="reg-bg.jpg" alt="image" class="register__bg">
+if(isset($_GET['error']) && $_GET['error'] === 'email_in_use') {
+    js_alert("Email already in use!");
+}
+
+// Check if there's a success flag in the URL
+if(isset($_GET['success']) && $_GET['success'] === 'registration_successful') {
+    js_alert("Registration Successful!");
+}
+?>
+        <img src="reg.png" alt="image" class="register__bg">
         <form action="" class="register__form" method="post">
             <h1 class="register__title">Register</h1>
 
@@ -65,12 +74,11 @@
                 </div>
             </div>
 
-            <button type="submit" id="register_button" name="sumbit" class="register__button">Register</button>
+            <button type="submit" id="register_button" name="submit" class="register__button">Register</button>
             <div class="login__register">
                Already have an account? <a href="login.php">Log-in</a>
             </div>
         </form>
-        <?php } ?>
     </div>
     <script src="register.js"></script>
 </body>
